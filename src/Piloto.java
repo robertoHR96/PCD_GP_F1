9635a292b415f52b6e9ab5520d20946ac4cb626e
Roberto Hermoso <109271873+robertoHR96@users.noreply.github.com>
2023-10-18 18:33:32 +0200

    Sincronizer init

--- src/Piloto.java
+++ src/Piloto.java
@@ -31,7 +31,7 @@ public class Piloto implements Comparable {
         long timeOld = this.timeEnd;
         anadirTiempoVuelta();
         long timeNew = this.timeEnd;
-        //
+        // aqui emepiza lo secuencial
         System.out.println("\uD83C\uDFCE\uFE0F Piloto: " + this.nombre + " -> \uD83D\uDD53 termino la vuelta: " + numVuelta + " en: " + (timeNew - timeOld) + "s - tiempo total: " + timeEnd + "s");
     }
 
